3e0e885a08cebb44c620ae0d60d4ff0ca1260893
ndech <ndech90@gmail.com>
2024-02-07 13:14:34 +0300

    Initialization

---
+++ backend/drf_course/urls.py
@@ -0,0 +1,30 @@
+"""
+URL configuration for drf_course project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from django.contrib import admin
+from core import views as core_views # new endpoint for '/contact/'
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+# router.register(r'contact', core_views.ContactAPIView, basename='contact') - incase the view is a viewset
+
+urlpatterns = router.urls
+
+urlpatterns += [
+    path('admin/', admin.site.urls),
+    path('contact/', core_views.ContactAPIView.as_view()),  # new endpoint for '/contact/'
+]
